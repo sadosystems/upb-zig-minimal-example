ee7e5193a44507d838a7ca83316f878e853e0b55
wmurra <william.matthew.murray@gmail.com>
2026-02-09 19:26:01 -0800

    some more minor changes

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const upb_zig_minimal_example = @import("upb_zig_minimal_example");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("All your {s} are belong to us.\n", .{"asd"});
     try upb_zig_minimal_example.bufferedPrint();
 }
 
@@ -12,15 +12,4 @@ test "simple test" {
     defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
