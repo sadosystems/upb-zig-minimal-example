50dfc549fc5dcc219d15b69a62e3ce0ff81c3743
wmurra <william.matthew.murray@gmail.com>
2026-02-09 19:31:08 -0800

    delete stuff we dont need

---
+++ README.md
@@ -0,0 +1,10 @@
+# μpb-Zig Minimal Example   
+This is a minimal example of the use of upb-zig in a Zig project.
+
+If you haven't installed the `protoc` compiler, follow the instructions in [Protocol Buffer Compiler Installation](https://protobuf.dev/installation/).
+If you are new to `Zig`, follow the instructions in [Zig Getting Started](https://ziglang.org/learn/getting-started/).
+
+Run
+```shell
+zig zig build run
+```
--- src/main.zig
+++ src/main.zig
@@ -6,10 +6,3 @@ pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"asd"});
     try upb_zig_minimal_example.bufferedPrint();
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
