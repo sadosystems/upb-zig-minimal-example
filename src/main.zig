c0d2a9499f0bcf62a37f9b694d4a2eeba9cedd99
wmurra <william.matthew.murray@gmail.com>
2026-02-12 20:15:43 -0800

    remove zig init stuff

--- src/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const upb_zig_minimal_example = @import("upb_zig_minimal_example");
-
-pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"asd"});
-    try upb_zig_minimal_example.bufferedPrint();
-}
